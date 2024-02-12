d958aafe033d316ad1d5284b4881d67bb751fb97
alanff97 <alan.figueroa.cl@gmail.com>
2024-02-12 21:14:47 +0100

    fetching data

--- secondPractice/src/App.jsx
+++ secondPractice/src/App.jsx
@@ -1,37 +1,70 @@
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
-import responseMovies from './mocks/with-results.json';
-import withoutResults from './mocks/no-results.json';
+import Movies from './components/Movies';
+import { useMovies } from './hooks/useMovies';
+
+function useSearch() {
+  const [search, updateSearch] = useState('');
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === '';
+      return;
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una película vacía');
+      return;
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número');
+      return;
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres');
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  return { search, updateSearch, error };
+}
 
 function App() {
-  const movies = responseMovies.Search;
-  const hasMovies = movies?.length > 0;
+  const { search, updateSearch, error } = useSearch();
+  const { movies: mappedMovies, getMovies } = useMovies({ search });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getMovies();
+  };
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value);
+  };
+
   return (
     <div className="page">
       <header>
         <h1>Movie Search</h1>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <input
+            onChange={handleChange}
             type="text"
             placeholder="Avengers, Star Wars, The Godfather..."
           />
           <button type="submit">Search</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
-        {hasMovies ? (
-          <ul>
-            {movies.map((movie) => (
-              <li key={movie.imdbID}>
-                <h3>{movie.Title}</h3>
-                <p>{movie.Year}</p>
-                <img src={movie.Poster} alt={movie.Title} />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>{withoutResults.Response}</p>
-        )}
+        <Movies movies={mappedMovies} />
       </main>
     </div>
   );
---
+++ secondPractice/src/hooks/useMovies.js
@@ -0,0 +1,27 @@
+//import withResults from '../mocks/with-results.json';
+import withoutResults from '../mocks/no-results.json';
+
+import { useState } from 'react';
+
+export function useMovies({ search }) {
+  const [responseMovies, setResponseMovies] = useState([]);
+
+  const movies = responseMovies.Search;
+  const mappedMovies = movies?.map((movie) => ({
+    id: movie.imdbID,
+    title: movie.Title,
+    year: movie.Year,
+    poster: movie.Poster,
+  }));
+
+  const getMovies = () => {
+    if (search) {
+      fetch(`https://www.omdbapi.com/?apikey=2bcb2bae&s=${search}`)
+        .then((res) => res.json())
+        .then((json) => setResponseMovies(json));
+    } else {
+      setResponseMovies(withoutResults);
+    }
+  };
+  return { movies: mappedMovies, getMovies };
+}
