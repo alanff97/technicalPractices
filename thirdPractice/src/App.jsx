e0fca0b55ac7749b6ad9a771b4d4bd00b6e732c3
alanff97 <alan.figueroa.cl@gmail.com>
2024-02-20 10:31:35 +0100

    filters complete

--- thirdPractice/src/App.jsx
+++ thirdPractice/src/App.jsx
@@ -2,10 +2,11 @@ import { products as initialProducts } from './mocks/products.json';
 import { useState } from 'react';
 import { Products } from './components/Products';
 import { Header } from './components/Header';
+import { useFilters } from './hooks/useFilters';
 
 function App() {
   const [products] = useState(initialProducts);
-  const { filters, filterProducts, setFilters } = useFilters();
+  const { filterProducts } = useFilters();
 
   const filteredProducts = filterProducts(products);
 
