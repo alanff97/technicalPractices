4ebb34ba7ce9c9f5a559ab2406f6fd29e3aa48b2
alanff97 <alan.figueroa.cl@gmail.com>
2024-02-20 10:26:03 +0100

    config provider and changes in APP

--- thirdPractice/src/App.jsx
+++ thirdPractice/src/App.jsx
@@ -1,5 +1,20 @@
+import { products as initialProducts } from './mocks/products.json';
+import { useState } from 'react';
+import { Products } from './components/Products';
+import { Header } from './components/Header';
+
 function App() {
-  return <h1>Shopping Cart</h1>;
+  const [products] = useState(initialProducts);
+  const { filters, filterProducts, setFilters } = useFilters();
+
+  const filteredProducts = filterProducts(products);
+
+  return (
+    <>
+      <Header />
+      <Products products={filteredProducts} />
+    </>
+  );
 }
 
 export default App;
